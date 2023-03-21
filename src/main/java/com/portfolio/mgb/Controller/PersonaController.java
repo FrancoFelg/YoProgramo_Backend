e9c093769dc389d8361eddc30ccdb16d0884c2d9
FrancoFelg <franco_lamberti_02@yahoo.com.ar>
2023-03-20 23:51:57 -0300

    Probando Deploy 1

--- src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-frontend-argprog.web.app")
 @RequestMapping("/personas")
 public class PersonaController {
 
